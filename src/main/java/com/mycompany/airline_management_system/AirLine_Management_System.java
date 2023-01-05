088ac452cf1053ae02b29810bb5e92c361d98cd1
SHRISHTY MALVIYYA <malviya.shrishty@gmail.com>
2023-01-05 10:23:31 +0530

    Adding airline management system code

---
+++ src/main/java/com/mycompany/airline_management_system/AirLine_Management_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airline_management_system;
+
+/**
+ *
+ * @author Shrishty
+ */
+public class AirLine_Management_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
